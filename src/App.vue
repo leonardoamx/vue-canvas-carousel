a03fbd30ddef2e037b6575b33214c251b7c8e5f6
Leonardo Molina <leonardoamx@outlook.com>
2026-02-18 03:08:38 -0600

    Update demo page

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import { ref } from 'vue';
 import CanvasImageCarousel from './components/CanvasImageCarousel.vue';
 
 const imagesList = ref([
-  "https://picsum.photos/2000/1000",
   "https://picsum.photos/2000/1500",
+  "https://picsum.photos/2000/1000",
   "https://picsum.photos/2000/4000",
   "https://picsum.photos/2000/3000",
   "https://picsum.photos/2000/2000",
@@ -13,12 +13,30 @@ const imagesList = ref([
 </script>
 
 <template>
-  <h1>Canvas image carousel demo</h1>
-  <p>
-    Objective: Render a set of images on a canvas element and create a carousel effect by transitioning between them. The carousel should automatically cycle through the images every few seconds, and users should also have the option to manually navigate through the images using buttons or swipe gestures.
-  </p>
+  <div class="container">
+    <h1>Canvas image carousel demo</h1>
+    <p>
+      Objective: Render a set of images on a canvas element and create a carousel.
+      <br>
+      Users should have the option to manually navigate through the images using swipe gestures.
+    </p>
 
     <CanvasImageCarousel :images="imagesList" :width="800" :height="500" />
+
+    <h2>Takeaways</h2>
+    <p>
+      Using the canvas API brought me back to my days using Flash Platform and distributing elements in the stage.
+      A major feature I would like to implement next is improving the drag & drop process, adding snap to picture and inertia features, with Greensock animation engine.
+    </p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .container {
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
+    font: 12pt Aptos, Lato, Helvetica, Arial;
+    color: hsl(200, 100%, 20%);
+  }
+</style>
